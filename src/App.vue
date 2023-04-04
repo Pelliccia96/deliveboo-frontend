d62f6406faad0dd5c5c97b00be95f69d433558ed
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-04-04 19:32:17 +0200

    Adds main structure

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  
+    <AppHeader/>
+    <div class="main-page">
+      <router-view></router-view>
+      <AppFooter class="" />
+    </div>
+
+</template>
+
+<script>
+import { store } from "./stores/store";
+import AppHeader from './components/AppHeader.vue'; 
+import AppFooter from './components/AppFooter.vue'; 
+export default {
+  components: { AppHeader, AppFooter },
+  data() {
+    return {};
+  },
+  mounted() {
+    store.fn.loadStorage()
+  },
+  beforeUnmount(){
+    store.fn.saveStorage()
+  }
+};
+</script>
+
+<style lang="scss">
+@use "./styles/main.scss";
+.main-page{
+  height: calc(100vh - 74px);
+  
+}
+</style>
